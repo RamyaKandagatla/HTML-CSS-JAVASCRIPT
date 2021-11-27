707d04be9b2b558073096a8031b658fc126f8eb3
RAMYA <kandagatlaramya5@gmail.com>
2021-11-27 15:27:14 +0530

    commit-1

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,15 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if (!hasDrivingLiscence){
+        return "You cannot drive"
+    }
+    else if(isTired){
+        return "You shouldn't drive"
+    }else if(!isSober){
+        return "You shouldn't drive"
+    }else{
+        return "You can drive"
+    }
 }
 
 module.exports = CanDrive;
