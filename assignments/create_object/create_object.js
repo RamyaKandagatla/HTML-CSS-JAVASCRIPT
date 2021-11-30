e17c666f3722c484220c44c9bd2ab57f46b7142d
RAMYA <kandagatlaramya5@gmail.com>
2021-11-30 15:17:21 +0530

    play_with_array

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,10 +1,5 @@
 function CreateObject(arr) {
     // Write your code here
-    const obj={};
-    for(var i=1;i<arr.length;i=i+2){
-        obj[arr[i-1]]=arr[i];
-    }
-    return obj
 }
 
 module.exports = CreateObject;
